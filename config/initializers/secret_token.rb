74c205b8bad1694034335bb9802923a70b151ef8
Brent Morrow <brent@brentmorrow.com>
2011-10-10 14:57:32 -0500

    adding project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrentmorrowCom::Application.config.secret_token = '0defea389c19a7448a03f93c00fdd62b81fb135024a74efb7b220cc26ada9b66237efa8e599e71562068ee3e1c5cb7553b767a34284544e41a0f730896d9135d'
